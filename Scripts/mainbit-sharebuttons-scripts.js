8d503e86aa2db7019e7d5464bf28f814b4d2f6dc
neTp9c <alexpetryakov@gmail.com>
2014-09-29 23:54:12 +0400

    Change to yandex share

---
+++ Scripts/mainbit-sharebuttons-scripts.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    // share buttons
+    // http://api.yandex.ru/share/doc/dg/concepts/share-button-ov.xml
+    if (typeof Ya !== 'undefined') {
+        var ya_share = $('#ya_share');
+        var YaShareInstance = new Ya.share({
+            element: 'ya_share',
+            elementStyle: {
+                type: 'none',
+                quickServices: ['odnoklassniki', 'vkontakte', 'facebook', 'twitter', 'gplus', 'linkedin']
+            },
+            //can be override only title or link
+            //serviceSpecific: {
+            //    linkedin: {
+            //        image: ya_share.data('yashare-linkedin-image')
+            //    }
+            //}
+        });
+    }
+})
